b1dffa67316e0d2e6f7fb1040b19d3a575d68028
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-06 13:23:17 +0100

    Add StartQuiz function in MainPage.xaml.cs

--- projektGit/projektGit/MainPage.xaml.cs
+++ projektGit/projektGit/MainPage.xaml.cs
@@ -27,5 +27,24 @@ namespace projektGit
             Random random = new Random();
             questions = Enumerable.Range(0, 5).Select(_=> random.Next(1,1001)).ToList();
         }
+
+        private void StartQuiz(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(userNameEntry.Text))
+            {
+                DisplayAlert("Alert", "pole nie może być puste!", "OK");
+            }
+            else
+            {
+                currentScore = 0;
+                currentQusetionIndex = 0;
+                times.Clear();
+                startGameView.IsVisible = false;
+                finishGameView.IsVisible = false;
+                gameView.IsVisible = true;
+                ShowNextQuestion();
+
+            }
+        }
     }
 }
