840ed774b26f597ac460af0c90fd9f6c5dd59f31
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-06 13:15:49 +0100

    Add entire Database class

--- projektGit/projektGit/Database.cs
+++ projektGit/projektGit/Database.cs
@@ -16,9 +16,34 @@ namespace projektGit
             database.CreateTableAsync<UserResult>().Wait();
         }
 
-        public Task<List<UserResul>> GetResultsAsync()
+        public Task<List<UserResult>> GetResultsAsync()
         {
             return database.Table<UserResult>().ToListAsync();
+        }
+
+        public Task<UserResult> GetUserName(string userName)
+        {
+            return database.Table<UserResult>().Where(i => i.UserName == userName).FirstOrDefaultAsync();
+        }
+
+        public async Task SaveResultAsync(UserResult result)
+        {
+            var existingResult = await GetUserName(result.UserName);
+
+            if(existingResult !=null)
+            {
+                if(result.Score > existingResult.Score) 
+                {
+                    existingResult.TotalTime = result.TotalTime;
+                    existingResult.Score = result.Score;
+                    await database.UpdateAsync(existingResult);
+                }
+            }
+            else
+            {
+                await database.InsertAsync(result);
+            }
+
         }
     }
 }
