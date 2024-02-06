a360694ff00f5221217ffdfa164778c998f53453
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-06 12:59:22 +0100

    Add constructor in Database class

---
+++ projektGit/projektGit/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projektGit
+{
+    public class Database
+    {
+        private SQLiteAsyncConnection(dbPath);
+
+        public Database(string dbPath)
+        {
+            database = new SQLLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<UserResult>().Wait();
+        }
+
+
+    }
+}
