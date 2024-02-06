4043e31d2b2dcf59df166ec9844507dce3a95ca3
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-06 13:11:37 +0100

    Add UserResult class

--- projektGit/projektGit/Database.cs
+++ projektGit/projektGit/Database.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
+using System.Threading.Tasks;
 
 namespace projektGit
 {
     public class Database
     {
-        private SQLiteAsyncConnection(dbPath);
+        private SQLiteAsyncConnection database;
 
         public Database(string dbPath)
         {
-            database = new SQLLiteAsyncConnection(dbPath);
+            database = new SQLiteAsyncConnection(dbPath);
             database.CreateTableAsync<UserResult>().Wait();
         }
 
-
+        public Task<List<UserResul>> GetResultsAsync()
+        {
+            return database.Table<UserResult>().ToListAsync();
+        }
     }
 }
