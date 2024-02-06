1c5668cd828284f92c1e88b49bb3cba0288c8edf
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-06 13:18:13 +0100

    Add global variables and lists in mainPage.xaml.cs

--- projektGit/projektGit/MainPage.xaml.cs
+++ projektGit/projektGit/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,15 @@ namespace projektGit
 {
     public partial class MainPage : ContentPage
     {
+        private int currentQusetionIndex = 0;
+        private int currentScore = 0;
+        private List<int> questions = new List<int>();
+        private Stopwatch stopwatch = new Stopwatch();
+        private List<double> times = new List<double>();
         public MainPage()
         {
             InitializeComponent();
+            
         }
     }
 }
